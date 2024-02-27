3b82297efc3ef6d58a7ce9c8bd1619dff148f11c
Alexander Korochkin <ronogor228@gmail.com>
2024-02-27 17:06:46 +0300

    start_diploma_task-1

---
+++ unit_tests/constants.ts
@@ -0,0 +1,13 @@
+
+
+export enum Errors {
+    INVALIDEMAIL = "You entered an invalid email address",
+    INVALIDPASSWORD = "You entered an invalid password",
+    INVALIDUSERNAME = "Username must contain at least 1 of any symbol",
+    INVALIDAGE = "The age cannot be more than 150 or less than 0",
+    INVALIDTERMSAGREEMENT = "You have not agreed to the terms of use"
+}
+
+export enum SuccessMessages {
+    SUCCESSREGISTRATION = "You have successfully registered"
+}
---
+++ unit_tests/test/registrationForm.test.ts
@@ -0,0 +1,11 @@
+import RegistrationForm from "../registrationForm";
+
+
+describe("Test registration form", () => {
+    beforeAll(() => {
+        let reg = new RegistrationForm("", "", "", 0);
+    });
+    beforeEach(() => {
+        
+    })
+});
