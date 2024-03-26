a2830b2760f4de4948417afd0ba4ec07bdcf33e5
Alexander Korochkin <ronogor228@gmail.com>
2024-03-26 13:56:25 +0300

    add 10 without logic

---
+++ step-definitions/comparisonProduct.steps.ts
@@ -0,0 +1,43 @@
+import { Given, When, Then } from '@wdio/cucumber-framework';
+import { expect } from '@wdio/globals';
+
+
+When(/I choose a section "Телевизоры"/, async () => {
+
+});
+
+When(/I click on the name of the first TV /, async () => {
+
+});
+
+Then(/I see product page/, async () => {
+
+});
+
+When(/I mark the "Добавить к сравнению" checkbox/, async () => {
+
+});
+
+Then(/I see the checkbox is marked, the "1 товар в сравнении" bar appeared/, async () => {
+
+});
+
+When(/I'm going back to the list with all the TVs/, async () => {
+
+});
+
+When(/I click on the name of the second TV/, async () => {
+
+});
+
+Then(/The TV page is open. After being added to the comparison, there are already "2 товара в сравнении" on the bar/, async () => {
+
+});
+
+When(/I click on the pop-up that appears with the name "2 товара в сравнении"/, async () => {
+
+});
+
+Then(/I see page "Сравнение товаров", 2 TVs that I chose earlier. The differing characteristics should be highlighted in orange/, async () => {
+
+});
