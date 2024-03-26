17b4594806b93464453a1cedd6dc7be441eb6ebb
Alexander Korochkin <ronogor228@gmail.com>
2024-03-26 10:58:57 +0300

    add 7 feature

---
+++ step-definitions/currencyConverter.steps.ts
@@ -0,0 +1,35 @@
+import { Given, When, Then } from '@wdio/cucumber-framework';
+import { expect } from '@wdio/globals';
+
+
+When(/I will click on the link with the dollar exchange rate on the main page/, async () => {
+
+});
+
+Then(/I see The "Лучшие курсы валют" page is open, today's date is displayed, the exchange rate sections for USD, EUR, RUB/, async () => {
+
+});
+
+When(/I will click the "Купить" button in the converter/, async () => {
+
+});
+
+When(/Try to enter text in the converter field/, async () => {
+
+});
+
+Then(/I see the value of the field has not changed, the standard "100" is displayed/, async () => {
+
+});
+
+When(/I will enter a value in the converter field/, async () => {
+
+});
+
+When(/Choose the EUR currency/, async () => {
+
+});
+
+Then(/The value in BYN is calculated in the converter on the right./, async () => {
+
+});
